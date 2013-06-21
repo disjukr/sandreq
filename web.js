8b92778c0f8b0ce4b9fa08933cc2954930f6e48b
JongChan Choi <disjukr@naver.com>
2013-06-21 18:34:59 +0900

    pipe

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var request = require('request');
+var stream = require ("stream");
 
 var hostname = '0.0.0.0';
 var port = 1234;
@@ -11,25 +12,35 @@ http.createServer(function (req, res) {
         _response(404, 'text/plain', '404');
     }
     else {
-        request({uri: uri},
-        function (err, res, content) {
+        request(uri,
+        function (err, res, body) {
             if (err)
                 _response(404, 'text/plain', '404');
             else
                 _response(res.statusCode,
-                    res.headers['content-type'], content);
+                    res.headers['content-type'], body);
         });
     }
 }).listen(port, hostname);
 
 console.log('running on ' + hostname + ' port ' + port);
 
-function response(uri, res, statusCode, contentType, content) {
+function response(uri, res, statusCode, contentType, body) {
+    var contentStream = new stream.Readable();
+    var offset = 0;
+    contentStream._read = function (size) {
+        while (size--) {
+            contentStream.push(body[offset++]);
+            if (offset > body.length)
+                return;
+        }
+    }
     res.writeHead(statusCode, {
         'Content-Type': contentType,
+        'Content-Length': body.length,
         'Access-Control-Allow-Origin': '*'
     });
-    res.end(content);
+    contentStream.pipe(res);
 
     console.log(uri + ' -> ' + statusCode + ', ' + contentType);
 }

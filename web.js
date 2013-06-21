c5f41228382d8cafc5f103f626a0681e302631a2
JongChan Choi <disjukr@naver.com>
2013-06-21 22:35:16 +0900

    Remove dependancy

--- web.js
+++ web.js
@@ -1,46 +1,35 @@
 var http = require('http');
-var request = require('request');
-var stream = require ("stream");
 
 var hostname = '0.0.0.0';
 var port = 1234;
 
 http.createServer(function (req, res) {
     var uri = decodeURIComponent(req.url.substr(1));
-    var _response = response.bind(null, uri, res);
+    var _writeHead = writeHead.bind(null, uri, res);
+    var _res = res;
     if (uri.substr(0, 4) != 'http') {
-        _response(404, 'text/plain', '404');
+        _writeHead(404, 'text/plain');
+        _res.end('404');
     }
     else {
-        request(uri,
-        function (err, res, body) {
-            if (err)
-                _response(404, 'text/plain', '404');
-            else
-                _response(res.statusCode,
-                    res.headers['content-type'], body);
+        http.get(uri, function (res) {
+            _writeHead(res.statusCode,
+                    res.headers['content-type'], 'asdf');
+            res.pipe(_res);
+        }).on('error', function (err) {
+            _writeHead(404, 'text/plain');
+            _res.end('404');
         });
     }
 }).listen(port, hostname);
 
 console.log('running on ' + hostname + ' port ' + port);
 
-function response(uri, res, statusCode, contentType, body) {
-    var contentStream = new stream.Readable();
-    var offset = 0;
-    contentStream._read = function (size) {
-        while (size--) {
-            contentStream.push(body[offset++]);
-            if (offset > body.length)
-                return;
-        }
-    }
+function writeHead(uri, res, statusCode, contentType) {
     res.writeHead(statusCode, {
         'Content-Type': contentType,
-        'Content-Length': body.length,
         'Access-Control-Allow-Origin': '*'
     });
-    contentStream.pipe(res);
 
     console.log(uri + ' -> ' + statusCode + ', ' + contentType);
 }
